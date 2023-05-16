6475078c666888a106346339452daf29135fca82
meryroconnor <meryroconnor@gmail.com>
2023-05-15 22:06:11 -0300

    gitignore actualization

--- main.py
+++
@@ -1,9 +0,0 @@
-
-from datetime import datetime
-from redshift_utils import request_data,conn_redshift,load_to_redshift
-
-if __name__ == "__main__":
-    today = datetime.today()
-    data = request_data(date=today)
-    conn_db_coder = conn_redshift(".env/pwd_coder.txt")
-    load_to_redshift(conn=conn_db_coder, table_name="forecast",dataframe=data)
