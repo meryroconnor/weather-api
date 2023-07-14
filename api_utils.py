4a8f1e3cd3f7e4ebc5652b8dcbd6b0c69b251165
meryroconnor <meryroconnor@gmail.com>
2023-07-13 21:45:24 -0300

    cierre del tp

--- api_utils.py
+++ api_utils.py
@@ -11,27 +11,29 @@ def get_api_key(path):
     return key_api["pwd"]
 
 def request_forecast(date):
+    try:
+        key=get_api_key(".creds/api_key.txt")
+        base_url = "http://api.weatherapi.com/v1/history.json"
+        url = f"{base_url}?key={key}&q=Buenos Aires&dt={date}"
+        response = requests.get(url)
+        data = response.json()
+        latitud= data["location"]["lat"]
+        longitud= data["location"]["lon"]
+        city = "Buenos Aires"
+        country = "Argentina"
+        mintemp_c= data["forecast"]["forecastday"][0]["day"]["mintemp_c"]
+        maxtemp_c= data["forecast"]["forecastday"][0]["day"]["maxtemp_c"]
+        avgtemp_c = data["forecast"]["forecastday"][0]["day"]["avgtemp_c"]
+        avghumidity= data["forecast"]["forecastday"][0]["day"]["avghumidity"]
+        totalprecip_mm= data["forecast"]["forecastday"][0]["day"]["totalprecip_mm"]
+        description=data["forecast"]["forecastday"][0]["day"]["condition"]["text"]
 
-    key=get_api_key(".env/api_key.txt")
-    base_url = "http://api.weatherapi.com/v1/history.json"
-    url = f"{base_url}?key={key}&q=Buenos Aires&dt={date}"
-    response = requests.get(url)
-    data = response.json()
-    latitud= data["location"]["lat"]
-    longitud= data["location"]["lon"]
-    city = "Buenos Aires"
-    country = "Argentina"
-    mintemp_c= data["forecast"]["forecastday"][0]["day"]["mintemp_c"]
-    maxtemp_c= data["forecast"]["forecastday"][0]["day"]["maxtemp_c"]
-    avgtemp_c = data["forecast"]["forecastday"][0]["day"]["avgtemp_c"]
-    avghumidity= data["forecast"]["forecastday"][0]["day"]["avghumidity"]
-    totalprecip_mm= data["forecast"]["forecastday"][0]["day"]["totalprecip_mm"]
-    description=data["forecast"]["forecastday"][0]["day"]["condition"]["text"]
-
-    df = pd.DataFrame([{"date":date,"lat":latitud,"long":longitud,"country":country, "city":city
-                        ,"mintemp_c":mintemp_c,"maxtemp_c":maxtemp_c, "avgtemp_c":avgtemp_c
-                        ,"avghumidity":avghumidity,"totalprecip_mm":totalprecip_mm,"description":description}])
-    return df
+        df = pd.DataFrame([{"date":date,"lat":latitud,"long":longitud,"country":country, "city":city
+                            ,"mintemp_c":mintemp_c,"maxtemp_c":maxtemp_c, "avgtemp_c":avgtemp_c
+                            ,"avghumidity":avghumidity,"totalprecip_mm":totalprecip_mm,"description":description}])
+        return df
+    except Exception as e:
+        print(e)
 
 
 
